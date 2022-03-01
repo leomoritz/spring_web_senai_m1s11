ec161c157dc6f946adb96425c268d15b96a96435
Leônidas Moritz Rosa <leonidas.rosa@senior.com.br>
2022-03-01 19:01:13 -0300

    projeto criado em aula

---
+++ src/main/java/com/devinhouse/exercicio/models/Cliente.java
@@ -0,0 +1,15 @@
+package com.devinhouse.exercicio.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+}
